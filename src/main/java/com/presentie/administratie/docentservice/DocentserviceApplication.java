e66e2480b5d52f94489a1b8b746645254190675a
Bakels <ricobakels2@hotmail.com>
2017-11-08 13:55:07 +0100

    first commit

---
+++ src/main/java/com/presentie/administratie/docentservice/DocentserviceApplication.java
@@ -0,0 +1,12 @@
+package com.presentie.administratie.docentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocentserviceApplication.class, args);
+	}
+}
